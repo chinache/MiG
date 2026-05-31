31734227ae38138c091ee829f46d2c7048063230
CHEN <chinachen.cc@gmail.com>
2026-05-31 13:17:35 +0800

    Refactor home business entry grid

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,46 @@
 Page({
-  goCalc: function () {
-    wx.navigateTo({
-      url: '/pages/calc/calc'
-    })
+  data: {
+    businessEntries: [
+      {
+        key: 'loan',
+        name: '贷款薪酬',
+        desc: '进入贷款薪酬基础页面',
+        icon: '贷',
+        theme: 'blue',
+        url: '/pages/loan/loan'
+      },
+      {
+        key: 'deposit',
+        name: '存款薪酬',
+        desc: '进入存款薪酬基础页面',
+        icon: '存',
+        theme: 'green',
+        url: '/pages/deposit/deposit'
+      },
+      {
+        key: 'insurance',
+        name: '保险薪酬',
+        desc: '进入保险薪酬基础页面',
+        icon: '保',
+        theme: 'orange',
+        url: '/pages/insurance/insurance'
+      },
+      {
+        key: 'gold',
+        name: '黄金薪酬',
+        desc: '进入黄金薪酬基础页面',
+        icon: '金',
+        theme: 'gold',
+        url: '/pages/gold/gold'
+      }
+    ]
   },
 
-  goAbout: function () {
+  goBusiness: function (event) {
+    var url = event.currentTarget.dataset.url
+
     wx.navigateTo({
-      url: '/pages/about/about'
+      url: url
     })
   }
 })
