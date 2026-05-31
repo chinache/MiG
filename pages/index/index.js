7c9952f43efd93167b03daaaf0a4b32fbbddaf52
CHEN <chinachen.cc@gmail.com>
2026-05-31 14:58:07 +0800

    Optimize home UI without binary assets

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,60 @@
 Page({
-  goCalc: function () {
-    wx.navigateTo({
-      url: '/pages/calc/calc'
+  data: {
+    showBrandLogo: true,
+    showMascot: true,
+    businessEntries: [
+      {
+        key: 'loan',
+        name: '贷款薪酬',
+        desc: '测算贷款业务收入',
+        icon: '贷',
+        theme: 'blue',
+        url: '/pages/loan/loan'
+      },
+      {
+        key: 'deposit',
+        name: '存款薪酬',
+        desc: '测算存款业务收入',
+        icon: '存',
+        theme: 'green',
+        url: '/pages/deposit/deposit'
+      },
+      {
+        key: 'insurance',
+        name: '保险薪酬',
+        desc: '测算保险业务收入',
+        icon: '保',
+        theme: 'purple',
+        url: '/pages/insurance/insurance'
+      },
+      {
+        key: 'gold',
+        name: '黄金薪酬',
+        desc: '测算黄金业务收入',
+        icon: '金',
+        theme: 'gold',
+        url: '/pages/gold/gold'
+      }
+    ]
+  },
+
+  onBrandLogoError: function () {
+    this.setData({
+      showBrandLogo: false
     })
   },
 
-  goAbout: function () {
+  onMascotError: function () {
+    this.setData({
+      showMascot: false
+    })
+  },
+
+  goBusiness: function (event) {
+    var url = event.currentTarget.dataset.url
+
     wx.navigateTo({
-      url: '/pages/about/about'
+      url: url
     })
   }
 })
